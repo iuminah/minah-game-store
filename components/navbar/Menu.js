b75ded387072648e541ada0510aae5085a1f3529
Minah <huytq.minah@gmail.com>
2023-01-28 04:10:53 +0700

    Fix Avatar

--- components/navbar/Menu.js
+++ components/navbar/Menu.js
@@ -21,16 +21,17 @@ import {
   selectUserData,
 } from "@/redux/accountSlice";
 import {DOMAIN, getUserData} from "@/libs/api";
+import userIcon from "../../public/favicon/userIcon.png";
 
 export default function MenuBar() {
   const dispatch = useDispatch();
   const [openNav, setOpenNav] = useState(false);
+  const [avatar, setAvatar] = useState("");
 
   const userLogged = useSelector(selectUserID);
   const userInfo = useSelector(selectUserData);
-  const userAvatar =
-    DOMAIN + userInfo?.[0]?.attributes.avatar.data.attributes.url || null;
-  const userName = userInfo?.[0]?.attributes.username || null;
+  const userAvatar = userInfo?.[0]?.attributes?.avatar?.data?.attributes?.url;
+  const userName = userInfo?.[0]?.attributes?.username;
 
   useEffect(() => {
     window.addEventListener(
@@ -42,15 +43,19 @@ export default function MenuBar() {
       dispatch(setUserData(userData));
     };
     userData();
-  }, [userLogged, dispatch]);
 
-  const logout = () => {
+    if (userAvatar) {
+      setAvatar(DOMAIN + userAvatar);
+    }
+  }, [userLogged, dispatch, userAvatar]);
+
+  const logout = useCallback(() => {
     dispatch(setUserID(null));
     dispatch(setToken(null));
-  };
+  }, [dispatch]);
 
   const navList = (
-    <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+    <ul className="mb-2 mt-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
       <Typography
         as="li"
         variant="small"
@@ -81,12 +86,35 @@ export default function MenuBar() {
           News
         </a>
       </Typography>
+      <span className="lg:hidden">
+        {userLogged ? (
+          <span className="">
+            <Menu placement="bottom-end">
+              <MenuHandler className="cursor-pointer">
+                <Avatar src={avatar || userIcon.src} alt="avatar" size="sm" />
+              </MenuHandler>
+              <MenuList>
+                {userName && <MenuItem>{userName}</MenuItem>}
+                <MenuItem onClick={logout} className="cursor-pointer">
+                  Logout
+                </MenuItem>
+              </MenuList>
+            </Menu>
+          </span>
+        ) : (
+          <Link href="/account/login">
+            <Button variant="gradient" size="sm" className="">
+              Sign in
+            </Button>
+          </Link>
+        )}
+      </span>
     </ul>
   );
 
   return (
-    <Navbar className="max-w-[100%] lg:px-8 lg:py-4 rounded-none ">
-      <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
+    <Navbar className="max-w-[100%] lg:px-8 lg:py-4 rounded-none text-blue-gray-900">
+      <div className="container mx-auto flex items-center justify-between ">
         <Typography
           as="a"
           href="#"
@@ -96,7 +124,7 @@ export default function MenuBar() {
           <span>Minah Game Store</span>
         </Typography>
         <div className="hidden lg:block">{navList}</div>
-        <div className="space-x-4 flex items-center">
+        <div className="lg:space-x-4 flex items-center">
           <IconButton
             variant="text"
             className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
@@ -135,10 +163,10 @@ export default function MenuBar() {
             )}
           </IconButton>
           {userLogged ? (
-            <span className="">
+            <span className="hidden lg:block">
               <Menu placement="bottom-end">
                 <MenuHandler className="cursor-pointer">
-                  <Avatar src={userAvatar} alt="avatar" />
+                  <Avatar src={avatar || userIcon.src} alt="avatar" size="sm" />
                 </MenuHandler>
                 <MenuList>
                   {userName && <MenuItem>{userName}</MenuItem>}
